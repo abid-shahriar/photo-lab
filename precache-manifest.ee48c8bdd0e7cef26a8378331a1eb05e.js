4e847e5395ac2bf190552e2570249bf4722068d7
abid7 <abidshahriar7@gmail.com>
2020-08-08 12:36:09 +0600

    Updates

---
+++ precache-manifest.ee48c8bdd0e7cef26a8378331a1eb05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244d4f85305b979ae547416b5a74545b",
+    "url": "/photo-lab/index.html"
+  },
+  {
+    "revision": "6af11c4f4a230ccd7b5f",
+    "url": "/photo-lab/static/css/main.0107a182.chunk.css"
+  },
+  {
+    "revision": "18bebf6f5f5c2bc18808",
+    "url": "/photo-lab/static/js/2.30715cd4.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/photo-lab/static/js/2.30715cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af11c4f4a230ccd7b5f",
+    "url": "/photo-lab/static/js/main.18ac882b.chunk.js"
+  },
+  {
+    "revision": "2dc4784b6fe4cc10d007",
+    "url": "/photo-lab/static/js/runtime-main.44b5287b.js"
+  }
+]);
